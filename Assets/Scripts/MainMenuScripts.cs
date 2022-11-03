bd196713c00e87d419bec7a477ce0c0f2f3b1662
ianmhenriquez <ianmhenriquez@hotmail.com>
2022-11-03 12:06:59 -0400

    added scene transitions

--- Assets/Scripts/MainMenuScripts.cs
+++ Assets/Scripts/MainMenuScripts.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScripts : MonoBehaviour{
+
+    public Animator transition;
+
     public void Close(){
         Application.Quit();
         Debug.Log("Game is exiting");
+    }
+
+    IEnumerator wait(string name){
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        changeScene(name);
     }
 
     public void changeScene(string name){
