5cb376bd3bbe6196cae0c24f1b1afce3d2864648
Yousef Majidi <y.majidin@gmail.com>
2022-12-05 14:33:01 -0500

    brought the deleted features back

--- Assets/Scripts/MainMenuScripts.cs
+++ Assets/Scripts/MainMenuScripts.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScripts : MonoBehaviour{
-
-
+    private int _width;
+    private int _height;
+    private string _resolution = "1920x1080";
+    private int _quality = 5;
     public Animator transition;
+
     void Start(){
         Scene scene = SceneManager.GetActiveScene();
         Debug.Log("Active scene is " + scene.name);
@@ -18,15 +22,43 @@ public class MainMenuScripts : MonoBehaviour{
     }
 
     public void ChangeSceneWTransition(string name){
-
         StartCoroutine(LoadLevel(name));
+    }
+
+    public void ChangeScreenResolution(string resolution){
+        int width = 0;
+        int height = 0;
+        string[] res = resolution.Split('x');
+        width = int.Parse(res[0]);
+        height = int.Parse(res[1]);
+        _width = width;
+        _height = height;
+
+    }
+
+    public void ChangeQuality(int qualityIndex){
+        _quality = qualityIndex;
+    }
+
+    public void SaveOptions(){
+        Screen.SetResolution(_width, _height, true);
+        QualitySettings.SetQualityLevel(_quality);
+        Debug.Log("Options are saved");
+    }
+
+    public void setToDefault(){
+        ChangeScreenResolution(_resolution);
+        ChangeQuality(5);
+        Debug.Log("Options are set to default");
     }
 
     IEnumerator LoadLevel(string name){
         transition.SetTrigger("Start");
+        Time.timeScale = 1f;
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(name);
         Debug.Log("Scene is changing to " + name);
     }
+
 }
 
