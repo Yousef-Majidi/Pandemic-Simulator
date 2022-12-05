df583be5e5c08130327f99813a511c0c828ea3c2
Yousef Majidi <y.majidin@gmail.com>
2022-12-05 12:43:00 -0500

    Fixed animation bug

--- Assets/Scripts/WanderAI.cs
+++ Assets/Scripts/WanderAI.cs
@@ -63,7 +63,8 @@ public class WanderAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _animator.SetBool("IsWalking", _isWalking);
+        _animator.SetBool("isWalking", _isWalking);
+        
         // set the rotation target of the character
         Quaternion rotationTarget = Quaternion.LookRotation(_targetWaypoint.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotationTarget, _rotSpeed * Time.deltaTime);
@@ -79,59 +80,3 @@ public class WanderAI : MonoBehaviour
     }
 }
 
-/*public class Mover : MonoBehaviour
-{
-    public List<Transform> waypoints = new List<Transform>();
-
-    private Transform targetWaypoint;
-
-    private int targetIndex = 0;
-
-    private float minDist = 0.5f;
-
-    private int lastIndex;
-
-    private float moveSpeed = 5.0f;
-    private float rotateSpeed = 3.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        lastIndex = waypoints.Count - 1;
-        targetWaypoint = waypoints[targetIndex];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-        Quaternion rotationTarget = Quaternion.LookRotation(targetWaypoint.position - transform.position);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotationTarget, rotateSpeed * Time.deltaTime);
-
-        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
-        checkDistance(distance);
-        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, moveSpeed * Time.deltaTime);
-    }
-
-    void checkDistance(float currDist)
-    {
-        if(currDist < minDist)
-        {
-            targetIndex++;
-            UpdateTarget();
-        }
-    }
-
-    void UpdateTarget()
-    {
-        if(targetIndex > lastIndex)
-        {
-            targetIndex = 0;
-        }
-        targetWaypoint = waypoints[targetIndex];
-    }
-
-}
-*/
