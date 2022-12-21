95182386a97179a045e7d05675a4783bf8e1b132
Yousef Majidi <y.majidin@gmail.com>
2022-12-21 14:45:42 -0500

    implemented the Virus class as a scriptable object

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -30,26 +30,6 @@ public class NPC : MonoBehaviour
 
     [Space]
 
-    [Header("The following attributes will be removed and retrieved from the Virus class when it is implemented")]
-
-    [SerializeField]
-    [Tooltip("Character Cough Rate: Chance of spreading infection by being near another NPC")]
-    private float _coughRate = 0.5f;
-
-    [SerializeField]
-    [Tooltip("Character Touch Rate: Chance of spreading infection by touching another NPC")]
-    private float _touchRate = 0.5f;
-
-    [SerializeField]
-    [Tooltip("Character Stamina Decay Rate")]
-    private float _staminaDecayRate = 0.5f;
-
-    [SerializeField]
-    [Tooltip("Character Health Decay Rate")]
-    private float _healthDecayRate = 0.5f;
-
-    [Space]
-
     [Header("Prefab assets that will be used to change the NPC's appearance")]
 
     [SerializeField]
@@ -63,31 +43,41 @@ public class NPC : MonoBehaviour
 
     private float _triggerCounter;
     private NavMeshAgent _agent;
-    // private Virus _virus = null;                      <-- To be implemented...
 
-    private void ChangeAsset()
+    [SerializeField]
+    private Virus _virus;
+
+    public bool IsInfected { get => _isInfected; set => _isInfected = value; }
+    public float Health { get => _health; set => _health = value; }
+    public float Stamina { get => _stamina; set => _stamina = value; }
+    public float Happiness { get => _happiness; set => _happiness = value; }
+
+    private void UpdateAsset()
     {
         if (_isInfected && _assetType == AssetType.Healthy)
         {
-            Debug.Log("Changing to infected asset");
             _assetType = AssetType.Infected;
             GameObject newAsset = Instantiate(_infectedAsset, transform.position, transform.rotation);
             newAsset.transform.parent = transform.parent;
             CopyTo(newAsset);
+            if (Random.Range(0f, 1f) < _virus.MutationChance)
+            {
+                Virus newVirus = ScriptableObject.CreateInstance<Virus>();
+                newVirus.Mutate();
+                _virus = newVirus;
+                Debug.Log("After mutation: Cough Rate: " + newVirus.CoughRate + " - Touch Rate: " + newVirus.TouchRate + " - Stamina Decay Rate: " + newVirus.StaminaDecayRate + " - Health Decay Rate: " + newVirus.HealthDecayRate + " - Mutation Chance: " + newVirus.MutationChance);
+            }
             Destroy(gameObject);
-            Debug.Log("Destorying: " + gameObject.name);
             return;
         }
 
         if (!_isInfected && _assetType == AssetType.Infected)
         {
-            Debug.Log("Changing to healthy asset");
             _assetType = AssetType.Healthy;
             GameObject newAsset = Instantiate(_healthyAsset, transform.position, transform.rotation);
             newAsset.transform.parent = transform.parent;
             CopyTo(newAsset);
             Destroy(gameObject);
-            Debug.Log("Destorying: " + gameObject.name);
             return;
         }
     }
@@ -99,52 +89,25 @@ public class NPC : MonoBehaviour
         otherNpc._health = _health;
         otherNpc._stamina = _stamina;
         otherNpc._assetType = _assetType;
-        otherNpc._coughRate = _coughRate;
-        otherNpc._touchRate = _touchRate;
         otherNpc._happiness = _happiness;
-        otherNpc._staminaDecayRate = _staminaDecayRate;
-        otherNpc._healthDecayRate = _healthDecayRate;
         otherNpc._triggerCounter = _triggerCounter;
-        otherNpc._agent = _agent;
+        otherNpc._virus = _virus;
         var currentDestination = gameObject.GetComponent<Navigation>().GetDestination();
         other.GetComponent<Navigation>().SetDestination(currentDestination);
     }
 
-    public bool IsInfected()
-    {
-        return _isInfected;
-    }
 
-    public float GetCoughRate()
-    {
-        return _coughRate;
-    }
 
-    public float GetTouchRate()
-    {
-        return _touchRate;
-    }
-
-    public float GetHealth()
-    {
-        return _health;
-    }
-
-    public float GetStamin()
-    {
-        return _stamina;
-    }
-
-    public void SetStamina()
+    private void UpdateStamina()
     {
         if (_agent.velocity.magnitude > 0)
-            _stamina -= _staminaDecayRate * Time.deltaTime;
+            _stamina -= _virus.StaminaDecayRate * Time.deltaTime;
     }
 
-    public void SetHealth()
+    private void UpdateHealth()
     {
         if (_isInfected)
-            _health -= _healthDecayRate * Time.deltaTime;
+            _health -= _virus.HealthDecayRate * Time.deltaTime;
 
         if (_health <= 0)
             Destroy(gameObject);
@@ -156,11 +119,11 @@ public class NPC : MonoBehaviour
         {
             ++_triggerCounter;
             NPC npc = other.gameObject.GetComponent<NPC>();
-            if (_triggerCounter == 4 && npc.IsInfected())
-                if (Random.Range(0f, 1f) < npc.GetTouchRate())
+            if (_triggerCounter == 4 && npc.IsInfected)
+                if (Random.Range(0f, 1f) < npc._virus.TouchRate)
                     _isInfected = true;
-                else if (npc.IsInfected())
-                    if (Random.Range(0f, 1f) < npc.GetCoughRate())
+                else if (npc.IsInfected)
+                    if (Random.Range(0f, 1f) < npc._virus.CoughRate)
                         _isInfected = true;
         }
     }
@@ -184,9 +147,9 @@ public class NPC : MonoBehaviour
 
     void Update()
     {
-        SetStamina();
-        SetHealth();
-        ChangeAsset();
+        UpdateStamina();
+        UpdateHealth();
+        UpdateAsset();
     }
 
 
@@ -194,7 +157,7 @@ public class NPC : MonoBehaviour
     void WriteToLogFile()
     {
         string path = "Logs/log.txt";
-        string message = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " -- Name: " + gameObject.name + ", Health: " + _health + ", Stamina: " + _stamina + ", is Infected: " + _isInfected + ", Cough Rate: " + _coughRate + " Touch Rate: " + _touchRate + ", Stamina Decay Rate: " + _staminaDecayRate + ", Health Decay Rate: " + _healthDecayRate + "\n";
+        string message = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " -- Name: " + gameObject.name + ", Health: " + _health + ", Stamina: " + _stamina + ", is Infected: " + _isInfected + ", Cough Rate: " + _virus.CoughRate + " Touch Rate: " + _virus.TouchRate + ", Stamina Decay Rate: " + _virus.StaminaDecayRate + ", Health Decay Rate: " + _virus.HealthDecayRate + "\n";
 
         using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(@path, true))
             logFile.WriteLine(message);
