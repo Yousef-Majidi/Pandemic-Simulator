d9ac33e1c048f8355be1898d3a6491282066ec36
suhailsameer <54902370+Syclic-ux@users.noreply.github.com>
2023-02-01 21:50:56 -0500

    Updated navigation.cs

--- Assets/Scripts/NPC/Navigation.cs
+++ Assets/Scripts/NPC/Navigation.cs
@@ -8,25 +8,58 @@ public class Navigation : MonoBehaviour
     [SerializeField]
     [Tooltip("The destination that the NPC moves to")]
     private Transform _destination;
+    private Transform _home;
 
     private NavMeshAgent _agent;
+    private NPC _npc;
     private Animator _animator;
-    private GameObject[] _destinations;
-    private int _noOfDest;
+
+    private LinkedList<GameObject> _commercials = new();
+    private LinkedList<GameObject> _medicals = new();
+
+    //private GameObject[] _commercials;
+    //private GameObject[] _residential;
+    //private GameObject[] _medical;
+
+    private int _noOfComm;
+    private int _noOfResi;
+    private int _noOfMedi;
+
+    //getters and setter
+
+    public Transform Destination { get => _destination; set => _destination = value; }
+    public Transform Home { get => _home; set => _destination = _home; }
+    //public LinkedList<GameObject> Commercials { get => _commercials; set => _commercials = value; }
+    //public GameObject[] Residential{ get => _residential; set => _residential= value; }
+    //public GameObject[] Medical { get => _medical; set => _medical = value; }
+    //private GameManager _gameManager;
+
+    // add getter and setter for all private members
 
     public void UpdateDestination(Transform newDest)
     {
         //_destination = newDest;
         if (_agent.velocity.magnitude == 0)
         {
-            _destination = _destinations[Random.Range(0,_noOfDest)].transform;
-            _agent.destination = _destination.position;
-        }
-    }
+            if (_npc.Health < 25)
+            {
+                //_destination = _medicals[Random.Range(0, _noOfMedi)].transform;
+                _destination = _medicals.ElementAt(Random.Range(0, _medicals.Count)).transform;
+                _agent.destination = _destination.position;
+            }
 
-    public Transform GetDestination()
-    {
-        return _destination;
+            else if (_npc.Stamina < 25)
+            {
+                _destination = _home;
+                _agent.destination = _destination.position;
+            }
+
+            else
+            {
+                _destination = _commercials.ElementAt(Random.Range(0, _commercials.Count)).transform;
+                _agent.destination = _destination.position;
+            }
+        }
     }
 
     private void UpdateAnimation()
@@ -41,11 +74,31 @@ public class Navigation : MonoBehaviour
 
     private void Awake()
     {
-        _destinations = GameObject.FindGameObjectsWithTag("Commercial");
-        print(_destinations[0].transform);
-        _noOfDest = _destinations.Length;
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        _commercials = _gameManager.CommercialDestinations;
+        GameObject[] commercialWaypoints = GameObject.FindGameObjectsWithTag("Commercial");
+
+        foreach (GameObject waypoint in commercialWaypoints)
+        {
+            _commercials.AddFirst(waypoint);
+        }
+
+        //_residentials = GameObject.FindGameObjectsWithTag("Residential");
+        //_noOfResi = _residential.Length;
+        //_home = _residential[0].transform;
+
+        GameObject[] medicalWaypoints = GameObject.FindGameObjectsWithTag("Medical");
+        _medicals = _gameManager.MedicalDestination;
+        foreach (GameObject waypoint in medicalWaypoints)
+        {
+            _medicals.AddFirst(waypoint);
+        }
+
+        //_noOfMedi = _medical.Length;
+
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
+        _npc = GetComponent<NPC>();
     }
 
     private void Update()
