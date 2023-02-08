d691fca1b3f040f00e0761537751d845a27401ea
Yousef <y.majidin@gmail.com>
2023-02-08 13:55:44 -0500

    Updated the prefabs to remove the touch detector collider and the scripts to reflect this change

--- Assets/Scripts/Virus/Virus.cs
+++ Assets/Scripts/Virus/Virus.cs
@@ -11,10 +11,6 @@ public class Virus : ScriptableObject
     private float _coughRate = 0.5f;
 
     [SerializeField]
-    [Tooltip("The chance of spreading infection by touching another NPC")]
-    private float _touchRate = 0.5f;
-
-    [SerializeField]
     [Tooltip("The rate at which the NPC's stamina decays")]
     private float _staminaDecayRate = 0.5f;
 
@@ -26,40 +22,14 @@ public class Virus : ScriptableObject
     [Tooltip("The chance of mutation when infecting another NPC")]
     private float _mutationChance = 0.5f;
 
-    public float CoughRate
-    {
-        get { return _coughRate; }
-        set { _coughRate = value; }
-    }
-
-    public float TouchRate
-    {
-        get { return _touchRate; }
-        set { _touchRate = value; }
-    }
-
-    public float StaminaDecayRate
-    {
-        get { return _staminaDecayRate; }
-        set { _staminaDecayRate = value; }
-    }
-
-    public float HealthDecayRate
-    {
-        get { return _healthDecayRate; }
-        set { _healthDecayRate = value; }
-    }
-
-    public float MutationChance
-    {
-        get { return _mutationChance; }
-        set { _mutationChance = value; }
-    }
+    public float CoughRate { get => _coughRate; set => _coughRate = value; }
+    public float StaminaDecayRate { get => _staminaDecayRate; set => _staminaDecayRate = value; }
+    public float HealthDecayRate { get => _healthDecayRate; set => _healthDecayRate = value; }
+    public float MutationChance { get => _mutationChance; set => _mutationChance = value; }
 
     public void Mutate()
     {
         _coughRate = Random.Range(0.01f, 0.1f);
-        _touchRate = Random.Range(0.01f, 0.1f);
         _staminaDecayRate = Random.Range(1f, 5f);
         _healthDecayRate = Random.Range(1f, 5f);
         _mutationChance = Random.Range(0.01f, 0.1f);
@@ -68,7 +38,6 @@ public class Virus : ScriptableObject
     public void Copy(Virus source)
     {
         _coughRate = source._coughRate;
-        _touchRate = source._touchRate;
         _staminaDecayRate = source._staminaDecayRate;
         _healthDecayRate = source._healthDecayRate;
         _mutationChance = source._mutationChance;
