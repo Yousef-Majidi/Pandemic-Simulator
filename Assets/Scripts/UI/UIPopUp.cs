7fa16fab9ad8e25e3d5b5e7d6eea7c4d65263d11
ianmhenriquez <ianmhenriquez@hotmail.com>
2023-03-31 13:44:15 -0400

    fixed issue where npc pop looked jittery

--- Assets/Scripts/UI/UIPopUp.cs
+++ Assets/Scripts/UI/UIPopUp.cs
@@ -26,18 +26,12 @@ public class UIPopUp : MonoBehaviour
         GameObject canvasTemp = GameObject.Find("CanvasPop");
         if (canvasTemp != null && _gameManager.TimeManager.TimeScale != 0)
         {
-            // get rid of old text
             GameObject text = GameObject.Find("PopUpText");
-            if (text != null)
-            {
-                Destroy(text);
-            }
-            // wait for a delay before adding text
-            Invoke("addText", 0.1f);
+            addText();
         }
 
         GameObject saveCanTemp = GameObject.Find("SaveCanvasPop");
-        if (saveCanTemp != null )
+        if (saveCanTemp != null)
         {
             if (_intervalMin <= _gameManager.TimeManager.InGameMinute && _intervalHour <= _gameManager.TimeManager.InGameHour && _intervalDay <= _gameManager.TimeManager.InGameDay)
             {
@@ -46,23 +40,23 @@ public class UIPopUp : MonoBehaviour
         }
     }
 
-     void OnMouseDown()
+    void OnMouseDown()
     {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
-                if(hit.collider.gameObject.CompareTag("NPC"))
+                if (hit.collider.gameObject.CompareTag("NPC"))
                 {
                     audio.Play();
                     NpcPopUp(hit.collider.gameObject);
                     isPopUp = true;
                 }
-                else 
+                else
                 {
-                    if(isPopUp)
+                    if (isPopUp)
                     {
                         GameObject canvasTemp = GameObject.Find("CanvasPop");
                         if (canvasTemp != null)
@@ -91,12 +85,28 @@ public class UIPopUp : MonoBehaviour
         canvas.AddComponent<Canvas>();
         canvas.AddComponent<CanvasScaler>();
         canvas.AddComponent<GraphicRaycaster>();
-        
+
 
         // create panel
         GameObject panel = new GameObject("PopUpPanel", typeof(RectTransform));
         panel.transform.SetParent(canvas.transform, false);
         panel.AddComponent<Image>();
+
+        Transform currentDestination = _tempObj.GetComponent<Navigation>().Destination;
+        string destination = currentDestination.ToString();
+        destination = destination.Substring(0, destination.Length - 23);
+        GameObject text = new GameObject("PopUpText", typeof(RectTransform));
+        text.transform.SetParent(panel.transform, false);
+        text.AddComponent<TextMeshProUGUI>();
+        text.GetComponent<RectTransform>().sizeDelta = new Vector2(200, 200);
+        text.GetComponent<TextMeshProUGUI>().text = "Name: " + _tempObj.GetComponent<NPC>().Name + "\nHealth: " + _tempObj.GetComponent<NPC>().Health.ToString("F2") + "\nStamina: " + _tempObj.GetComponent<NPC>().Stamina.ToString("F2") + "\n Happiness: " + _tempObj.GetComponent<NPC>().Happiness.ToString("F2") + "\nDestination: " + destination + "\nInfected: " + _tempObj.GetComponent<NPC>().IsInfected;
+        text.GetComponent<TextMeshProUGUI>().color = Color.black;
+        text.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
+        text.GetComponent<TextMeshProUGUI>().fontSize = 16;
+        text.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+        text.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+        text.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
+        text.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
 
         //create button
         GameObject exitButton = new GameObject("Button", typeof(RectTransform));
@@ -146,32 +156,22 @@ public class UIPopUp : MonoBehaviour
         buttonText.GetComponent<TextMeshProUGUI>().color = Color.black;
         buttonText.GetComponent<TextMeshProUGUI>().fontSize = 44;
         buttonText.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
-
-        addText();
     }
 
-    void addText(){
-        GameObject panel = GameObject.Find("PopUpPanel");
-        if (panel != null){
+    void addText()
+    {
+        GameObject text = GameObject.Find("PopUpText");
+        if (text != null)
+        {
             Transform currentDestination = _tempObj.GetComponent<Navigation>().Destination;
             string destination = currentDestination.ToString();
             destination = destination.Substring(0, destination.Length - 23);
-            GameObject text = new GameObject("PopUpText", typeof(RectTransform));
-            text.transform.SetParent(panel.transform, false);
-            text.AddComponent<TextMeshProUGUI>();
-            text.GetComponent<RectTransform>().sizeDelta = new Vector2(200, 200);
-            text.GetComponent<TextMeshProUGUI>().text = "Name: " + _tempObj.GetComponent<NPC>().Name + "\nHealth: " + _tempObj.GetComponent<NPC>().Health.ToString("F2") + "\nStamina: " + _tempObj.GetComponent<NPC>().Stamina.ToString("F2") + "\n Happiness: " + _tempObj.GetComponent<NPC>().Happiness.ToString("F2") +"\nDestination: " +destination + "\nInfected: " + _tempObj.GetComponent<NPC>().IsInfected;
-            text.GetComponent<TextMeshProUGUI>().color = Color.black;
-            text.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
-            text.GetComponent<TextMeshProUGUI>().fontSize = 16;
-            text.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
-            text.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
-            text.GetComponent<RectTransform>().pivot = new Vector2(0, 0);
-            text.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+            text.GetComponent<TextMeshProUGUI>().text = "Name: " + _tempObj.GetComponent<NPC>().Name + "\nHealth: " + _tempObj.GetComponent<NPC>().Health.ToString("F2") + "\nStamina: " + _tempObj.GetComponent<NPC>().Stamina.ToString("F2") + "\n Happiness: " + _tempObj.GetComponent<NPC>().Happiness.ToString("F2") + "\nDestination: " + destination + "\nInfected: " + _tempObj.GetComponent<NPC>().IsInfected;
         }
     }
 
-    public void SaveLoadPopUp(string text){
+    public void SaveLoadPopUp(string text)
+    {
         GameObject canvasTemp = GameObject.Find("SaveCanvasPop");
         if (canvasTemp != null)
         {
